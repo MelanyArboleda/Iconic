707ca0f7418a848399668db8c74dfdf28aa58816
Gabriel Arboleda <tolosa-321@hotmail.com>
2017-05-08 23:54:33 +0000

    auth.js edited online with Bitbucket

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 module.exports = {
-	isLogged : function (req, res, next) {
-		if (req.isAuthenticated()) {
-			next();
-		}else{
-			res.redirect('/');
-		}
+	isLogged : function (req, res, next) {	
+			if (req.isAuthenticated()) {
+				next();
+			}else{
+				res.redirect('/');
+			}		
 	},
 	isLoggedInd : function (req, res, next){
-		if (req.isAuthenticated()) {
-			res.redirect('/auth/singup');
-		}else{
-			next();
+		if(req.params[0] != "/auth/singup"){
+			if (req.isAuthenticated()) {
+				res.redirect('/auth/singup');
+			}else{
+				next();
+			}
 		}
 	}
 }
