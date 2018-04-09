81edd39b537422fac797a0b85c32c765e1691ac3
Gabriel Arboleda <tolosa-321@hotmail.com>
2018-04-09 18:53:02 +0000

    appConstants.js edited online with Bitbucket

--- public/js/constants/appConstants.js
+++ public/js/constants/appConstants.js
@@ -1,5 +1,5 @@
 (function(){
     angular.module("iconic").constant("APP_CONSTANT", {
-        LOCAL_SERVICE_ENDPOINT: 'http://localhost:3000/auth'
+        LOCAL_SERVICE_ENDPOINT: 'http://192.168.1.52:3000/auth'
     });
 })();
